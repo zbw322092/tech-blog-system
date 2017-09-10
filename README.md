464d7a39470902c4e0f835f85a67685d544826d5
Bowen Zhong <bz4e14@soton.ac.uk>
2017-09-10 21:41:16 +0800

    router handle using bookshelf

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Tech Blog System Development
 Server components
-  
-  entry
-    get init config
-    start server
-  
-  config
-    db config
-  
-  db
-    connection
-    model
+db
+Reference:
+[1] What did I learn this week? Knex.js & Bookshelf.js https://travishorn.com/what-did-i-learn-this-week-knex-js-bookshelf-js-95d3490e3a6f <br>
+
+Others:
+[1] listen EADDRINUSE while using nodejs?
+https://stackoverflow.com/a/30163868
+
+
   
--- routes/blog.js
+++ routes/blog.js
@@ -8,6 +8,42 @@ router.route('/')
       .then((result) => {
         res.json({ result });
       });
+  })
+  .post((req, res) => {
+    new Blog({
+      firstName: req.body.firstName || '',
+      lastName: req.body.lastName || '',
+      emailAddress: req.body.emailAddress || ''
+    })
+    .save()
+    .then((model) => {
+      res.json({ model });
+    });
+  });
+
+router.route('/:id')
+  .put((req, res) => {
+    Blog
+      .where('id', req.params.id)
+      .fetch()
+      .then((result) => {
+        result.save({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          emailAddress: req.body.emailAddress
+        })
+        .then((model) => {
+          res.json({ model });
+        });
+      });
+  })
+  .delete((req, res) => {
+    Blog
+      .where('id', req.params.id)
+      .destroy()
+      .then((model) => {
+        res.json({ model });
+      });
   });
 
 module.exports = router;
