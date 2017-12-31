efd1b09c0a110905300a584ba3f508c2646f175d
Bowen Zhong <bz4e14@soton.ac.uk>
2017-12-31 17:57:29 +0800

    debug

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const debug = require('debug')('boot:index');
 const Nconf = require('./core/server/config');
 const host = Nconf.get('server:host') || '127.0.0.1';
 const port = Nconf.get('server:port') || '6789';
 
 app.use('/', (req, res) => {
+  debug('Got a request');
   res.send('hi there');
 });
 
 app.listen(port, host, () => {
+  debug('Starting Server...');
   console.log(`server is host on ${host}:${port}`);
 });
